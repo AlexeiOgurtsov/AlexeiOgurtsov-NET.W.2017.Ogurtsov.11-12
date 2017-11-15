1adc216061ab0cf24cef7605cd8ffe9481ad58a0
Alex <alexei.ogurtsov@gmail.com>
2017-11-15 14:46:21 +0300

    werw

---
+++ 04/LogicGeneratorFibonacciNumbers/GeneratorFibonacciNumbers/GeneratorFibonacciNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeneratorFibonacciNumbers
+{
+    public class GeneratorFibonacciNumbers
+    {
+        /// <summary>
+        /// Generate the specified quantity.
+        /// </summary>
+        /// <returns>The generate.</returns>
+        /// <param name="n">Quantity.</param>
+        public static IEnumerable<int> Generate(int n)
+        {
+            if (n < 1) throw new ArgumentException($"{nameof(n)} is invalid!");
+
+            int previous = -1;
+            int next = 1;
+            int temporary = 0;
+            for (int i = 0; i < n; i++)
+            {
+                temporary = next;
+                yield return next += previous;
+                previous = temporary;
+            }
+        }
+
+    }
+}
