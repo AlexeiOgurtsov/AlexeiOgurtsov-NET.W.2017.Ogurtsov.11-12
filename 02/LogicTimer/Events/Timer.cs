908b884c1a7452da3ffca30e1c55186231ba16da
Alex <alexei.ogurtsov@gmail.com>
2017-11-15 17:29:34 +0300

    Add Logic of Timer

---
+++ 02/LogicTimer/Events/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace Events
+{
+    public class Timer
+    {
+        /// <summary>
+        /// Event to be started after time is out
+        /// </summary>
+        public event EventHandler TimerStoped;
+
+        /// <summary>
+        /// Time
+        /// </summary>
+        private int time;
+
+        /// <summary>
+        /// Initializes an instance of the timer with the passed parameter.
+        /// </summary>
+        /// <param name="time">Starting time for Timer (in sec)</param>
+        public Timer(int time)
+        {
+            this.time = time;
+        }
+
+        /// <summary>
+        /// Starting timer
+        /// </summary>
+        public void Start()
+        {
+            Thread.Sleep(time * 1000);
+            TimeIsOut(this, new EventArgs());
+
+        }
+
+        /// <summary>
+        /// The end of time
+        /// </summary>
+        private void TimeIsOut(object o, EventArgs args)
+        {
+            TimerStoped(o, args);
+        }
+    }
+}
